fefc8a16e4a60ad93c7ffffdfa7e7d814da39f49
Guilherme P Silva <guilpas@gmail.com>
2023-06-05 17:03:40 -0300

    Requisito 24 - requests para a API

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getMealDetailsById, getDrinkDetailsById } from '../services/api';
 
 class RecipeDetails extends Component {
+  state = {
+    product: {},
+  };
+
+  componentDidMount() {
+    this.fetchProduct();
+  }
+
+  fetchProduct = async () => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const {
+      history: {
+        location: { pathname },
+      },
+    } = this.props;
+
+    let product = {};
+    if (pathname.includes('/meals')) product = await getMealDetailsById(id);
+    else product = await getDrinkDetailsById(id);
+    this.setState({ product });
+  };
+
   render() {
+    const { product } = this.state;
     return (
-      <div>RecipeDetails</div>
+      <>
+        <div>RecipeDetails</div>
+        <div>{product.idMeal}</div>
+      </>
     );
   }
 }
+
+RecipeDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default RecipeDetails;
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,15 @@
 // arquivo para requisições a API
+
+export async function getMealDetailsById(id) {
+  const endpointMeal = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const response = await fetch(endpointMeal);
+  const data = await response.json();
+  return data.meals[0];
+}
+
+export async function getDrinkDetailsById(id) {
+  const endpointDrink = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const response = await fetch(endpointDrink);
+  const data = await response.json();
+  return data.drinks[0];
+}
