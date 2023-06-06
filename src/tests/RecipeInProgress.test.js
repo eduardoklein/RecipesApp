88328653115064ac5eb4c17923ca8b5b8be920ea
Guilherme P Silva <guilpas@gmail.com>
2023-06-06 19:22:51 -0300

    Requisito 43 - testes da pagina de receita em progresso

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -1,4 +1,7 @@
 import { waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { within } from '@testing-library/dom';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import { mockFetch } from './helpers/mockDataRecipeDetails';
@@ -6,43 +9,66 @@ import { mockFetch } from './helpers/mockDataRecipeDetails';
 const MEAL_PATH = '/meals/52882/in-progress';
 const DRINK_PATH = '/drinks/178319/in-progress';
 const RECIPE_TITLE = 'recipe-title';
-const FAVORITE_BTN = 'favorite-btn';
-const START_RECIPE_BTN = 'start-recipe-btn';
-const WHITE_HEART = 'http://localhost/whiteHeartIcon.svg';
-const BLACK_HEART = 'http://localhost/blackHeartIcon.svg';
-const COPIED_MSG = 'Link copied!';
-const writeText = jest.fn();
-
-Object.assign(navigator, {
-  clipboard: {
-    writeText,
-  },
-});
+const FINISH_RECIPE_BTN = 'finish-recipe-btn';
 
 describe('Testando a Tela de receita em progresso', () => {
   beforeEach(() => jest.spyOn(global, 'fetch').mockImplementation(mockFetch));
   afterEach(() => jest.restoreAllMocks());
 
   it('testando se receita de meal é renderizada', async () => {
-    renderWithRouterAndRedux(<App />, {}, MEAL_PATH);
+    const { history } = renderWithRouterAndRedux(<App />, {}, MEAL_PATH);
     await waitFor(() => {
       expect(screen.queryByText(/loading.../i)).not.toBeInTheDocument();
     }, 3000);
     expect(screen.getByTestId(RECIPE_TITLE).textContent).toBe('Three Fish Pie');
-    expect(screen.getAllByTestId(/-ingredient-step/i)).toHaveLength(14);
     expect(screen.getByTestId('recipe-category')).toBeInTheDocument();
     expect(screen.getByTestId('recipe-photo')).toBeInTheDocument();
     expect(screen.getByTestId('instructions')).toBeInTheDocument();
+    expect(screen.getByTestId(FINISH_RECIPE_BTN)).toBeDisabled();
+
+    const ingredients = screen.getAllByTestId(/-ingredient-step/i);
+    expect(ingredients).toHaveLength(14);
+    ingredients.forEach((ingredient) => { userEvent.click(ingredient); });
+
+    expect(screen.getByTestId(FINISH_RECIPE_BTN)).not.toBeDisabled();
+    userEvent.click(screen.getByTestId(FINISH_RECIPE_BTN));
+    expect(history.location.pathname).toBe('/done-recipes');
   });
   it('testando se receita de drink é renderizada', async () => {
-    renderWithRouterAndRedux(<App />, {}, DRINK_PATH);
+    const { history } = renderWithRouterAndRedux(<App />, {}, DRINK_PATH);
     await waitFor(() => {
       expect(screen.queryByText(/loading.../i)).not.toBeInTheDocument();
     }, 3000);
     expect(screen.getByTestId(RECIPE_TITLE).textContent).toBe('Aquamarine');
-    expect(screen.getAllByTestId(/-ingredient-step/i)).toHaveLength(3);
     expect(screen.getByTestId('recipe-category')).toBeInTheDocument();
     expect(screen.getByTestId('recipe-photo')).toBeInTheDocument();
     expect(screen.getByTestId('instructions')).toBeInTheDocument();
+    expect(screen.getByTestId(FINISH_RECIPE_BTN)).toBeDisabled();
+
+    const ingredients = screen.getAllByTestId(/-ingredient-step/i);
+    expect(ingredients).toHaveLength(3);
+    ingredients.forEach((ingredient) => { userEvent.click(ingredient); });
+
+    expect(screen.getByTestId(FINISH_RECIPE_BTN)).not.toBeDisabled();
+    userEvent.click(screen.getByTestId(FINISH_RECIPE_BTN));
+    expect(history.location.pathname).toBe('/done-recipes');
+  });
+  it('testando se a lista de ingredientes permanece apos atualizar a página', async () => {
+    const { history } = renderWithRouterAndRedux(<App />, {}, DRINK_PATH);
+    await waitFor(() => {
+      expect(screen.queryByText(/loading.../i)).not.toBeInTheDocument();
+    }, 3000);
+    let ingredients = screen.getAllByTestId(/-ingredient-step/i);
+    let ingredientCheckbox = within(ingredients[1]).getByRole('checkbox');
+    expect(ingredientCheckbox.checked).toBe(true);
+    userEvent.click(ingredients[1]);
+    expect(ingredientCheckbox.checked).toBe(false);
+    act(() => {
+      history.push('/');
+      history.push(DRINK_PATH);
+    });
+    ingredients = screen.getAllByTestId(/-ingredient-step/i);
+    ingredientCheckbox = within(ingredients[1]).getByRole('checkbox');
+    expect(ingredientCheckbox.checked).toBe(false);
   });
 });
