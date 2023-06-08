f24b0e11cb689b70f3bcd6b5867b3b44c5581d64
Vitor Senhorinho <vitorsenhorinho@gmail.com>
2023-06-08 12:35:42 -0300

    Ajeitando req 16-18

---
+++ src/tests/helpers/Footer.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { renderWhithRouterAndRedux } from './renderWithRouterAndRedux';
+import App from './.App';
+
+describe('Testes do componente Footer', () => {
+  it('Verifica se o Footer está sendo renderizado corretamente', () => {
+    renderWithRouter(
+      <App />,
+      { initialEntries: ['/meals'] },
+    );
+
+    const profileIcon = screen.getByRole('img', {
+      name: /perfil/i,
+    });
+
+    expect(profileIcon).toBeInTheDocument();
+  });
+
+  it('Verifica se, ao clicar no ícone de busca, é exibido um input', () => {
+    renderWithRouter(
+      <App />,
+      { initialEntries: ['/meals'] },
+    );
+
+    const drinkIcon = screen.getByRole('img', {
+      name: /bebida/i,
+    });
+
+    const mealsIcon = screen.getByRole('img', {
+      name: /comida/i,
+    });
+
+    expect(drinkIcon).toBeInTheDocument();
+    expect(mealsIcon).toBeInTheDocument();
+  });
+});
